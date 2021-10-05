c1d8b2ef578583a8133499b0636bdbe586c3291f
Joakim Hansen <joakimskaalevik@hotmail.com>
2021-10-05 20:53:56 +0200

    initial commit - experiment4

---
+++ Assignment5/Experiment4/src/test/java/no/hvl/dat250/Experiment4/Experiment4ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.Experiment4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Experiment4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
