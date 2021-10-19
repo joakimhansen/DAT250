291f4858bc70f94890597650d594b447899f0020
Joakim Hansen <joakimskaalevik@hotmail.com>
2021-10-19 14:25:30 +0200

    experiment3 - queue messaging

---
+++ Assignment7/Experiment3/src/main/java/NewTask.java
@@ -0,0 +1,27 @@
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTask {
+
+    private final static String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        boolean durable = false;
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
+            String message = String.join(" ", args);
+
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+
+    }
+}
